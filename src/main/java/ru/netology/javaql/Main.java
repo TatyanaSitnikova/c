d8ba8ecaf1f7da5068505ffcaa2a4bae4d6432f0
TatyanaSitnikova <soulofsmm@gmail.com>
2023-09-10 18:57:04 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaql/Main.java
@@ -0,0 +1,11 @@
+package ru.netology.javaql;
+
+import ru.netology.javaql.services.RestService;
+
+public class Main {
+    public static void main (String[] args) {
+        RestService service = new RestService();
+        int mounth = service.calculate(100000, 60000, 150000);
+        System.out.println(mounth);
+    }
+    }
---
+++ src/test/java/ru/netology/javaql/services/RestServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.javaql.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RestServiceTest {
+    @Test
+    public void calculateForMinIncome() {
+        RestService service = new RestService();
+
+        int actual = service.calculate(10000, 3000, 20000);
+        int expectes = 3;
+
+        Assertions.assertEquals(expectes, actual);
+    }
+    }
+
